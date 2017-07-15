9b1b0aa5b2ecc8f6a8166f6562d34150ac6b5c0f
Jasmine Quach <xjasminequach@gmail.com>
2017-07-15 00:04:57 -0500

    final commit

--- for-loops.js
+++ for-loops.js
@@ -1,4 +1,57 @@
-for (let i = 5; i < 120; i+=10) {
-  console.log( i )
+// Increment by 10
+for (let i = 5; i < 120; i += 10) {
+  console.log( i );
 }
+
+// Decrement by division
+for(let i = 4096; i >= 1; i /= 2) {
+  console.log( i );
+}
+
+// Array iteration
+let presidents = [
+  "1 was George Washington",
+  "2 was John Adams",
+  "3 was Thomas Jefferson"
+];
+
+for (let i = 0; i < presidents.length; i++) {
+  console.log("President #" + presidents[i] + ".");
+}
+
+let antSpecies = [
+  "argentine",
+  "army",
+  "bigHeaded",
+  "black",
+  "bull",
+  "carpenter",
+  "crazy",
+  "fire",
+  "glider",
+  "honeyPot",
+  "jackJumper"
+];
+
+for (let i = 0; i < antSpecies.length; i++) {
+  console.log( "Ant Species" + " is " + antSpecies[i] + ".");
+}
+
+// Object iteration
+let antSpecies = {
+  argentine: {},
+  army: {},
+  bigHeaded: {},
+  black: {},
+  bull: {},
+  carpenter: {},
+  crazy: {},
+  fire: {},
+  glider: {},
+  honeyPot: {},
+  jackJumper: {}
+}
+
+for (let prop in antSpecies) {
+  console.log();
 
